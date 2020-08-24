a138c0a7505e6bba5cec703f0e88e125879c7d82
RanjithKB <ranjithkb34@gmail.com>
2020-08-24 23:26:10 +0530

    Dashboard-product page work

--- dantisApp/ngdantis/src/app/app.module.ts
+++ dantisApp/ngdantis/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppLoaderComponent } from './app-loader/app-loader.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    NgbModule,
     CommonModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), // ToastrModule added
--- dantisApp/ngdantis/src/app/dashboard/dashboard.component.ts
+++ dantisApp/ngdantis/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('editModal') editModal : TemplateRef<any>;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  
   }
 
+  addNew() {
+    this.modalService.open(this.editModal);
+  }
 }
